987655f9ee45656d7a613f5f0b03449c614b816d
gourav sharma <gourav.101sharma@gmail.com>
2025-06-22 18:54:00 +0530

    updated comments + Dsa

---
+++ collections/linkList/ListIterator.java
@@ -0,0 +1,25 @@
+package collections.linkList;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListIterator {
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<String>();
+        list.add("Gourav");
+        list.add("Sourav");
+        list.add("NoName");
+
+        java.util.ListIterator<String> itr = list.listIterator();
+        // Forward traversal
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+        
+        // Backward traversal
+        while (itr.hasPrevious()) {
+            System.out.println(itr.previous());
+        }
+    }
+}
