2eb9a570194d38713f800dd5614c174bd7fa02ab
gourav sharma <gourav.101sharma@gmail.com>
2025-06-25 21:30:35 +0530

    DSA

---
+++ collections/generics/TypeCastingDemo.java
@@ -0,0 +1,30 @@
+package collections.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TypeCastingDemo {
+    /**
+     * A demonstration of using generics to prevent ClassCastException. The example
+     * shows how to create a list of strings, add elements to it, and retrieve them
+     * without having to perform explicit casting. It also shows how attempting to
+     * add an integer to the list will result in a compilation error, thanks to the
+     * use of generics.
+     *
+     * @param args
+     *            the command line arguments
+     */
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        list.add("Gourav");
+        list.add("Sourav");
+        //list.add(10); // this will give error "Class cast Exception" because we are adding int into a list of string
+                        //So to prevent this we use generics
+        String name = (String) list.get(0);
+        String name2 = (String) list.get(1);
+        System.out.println(name);
+        System.out.println(name2);
+       // String name3 = (String) list.get(2);
+
+    }
+}
