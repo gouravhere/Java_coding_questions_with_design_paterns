d15cfc6ea7b07ee136ad65433aa0a934864a8f1b
gourav sharma <gourav.101sharma@gmail.com>
2025-07-04 00:22:19 +0530

    DSA

--- DsaForYou/FindAllNumbersDisappearedInArray.java
+++ DsaForYou/FindAllNumbersDisappearedInArray.java
@@ -1,24 +1,32 @@
 package DsaForYou;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class FindAllNumbersDisappearedInArray {
     public static void main(String [] args){
 
         int [] nums = {5,6,4,4,2,1,2};
-        Set<Integer> seen = new HashSet<Integer>();
-        for(int num : nums){
-            seen.add(num);
-        }
-        List<Integer> missing = new ArrayList<>();
-        for(int i = 1; i <=nums.length;i++){
-            if(!seen.contains(i)){
-                missing.add(i);
-            }
-        }
+        //Normal way
+//        Set<Integer> seen = new HashSet<Integer>();
+//        for(int num : nums){
+//            seen.add(num);
+//        }
+//        List<Integer> missing = new ArrayList<>();
+//        for(int i = 1; i <=nums.length;i++){
+//            if(!seen.contains(i)){
+//                missing.add(i);
+//            }
+//        }
+//        System.out.println(missing);
+
+        //Using streams ---
+
+        Set<Integer> seen = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        List<Integer> missing = IntStream.rangeClosed(1,nums.length).
+                                filter(i->!seen.contains(i)).boxed().toList();
+
         System.out.println(missing);
     }
 
